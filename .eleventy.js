bf46629c740515780c2aace12e0d87c292b7c039
georgeunsworth <george@mortar.works>
2024-10-01 13:17:42 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -9,40 +9,48 @@ const { MultiWatching } = require("webpack");
 const env = process.env.ELEVENTY_ENV;
 
 module.exports = function(eleventyConfig) {
-  // static passthroughs
+  // Static passthroughs for files like CSS, JS, images
   eleventyConfig.addPassthroughCopy("src/site/static");
 
-  // yaml data files
+  // Enable YAML data files to be processed
   eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
 
-
+  // Add filters
   eleventyConfig.addFilter('date', dateFilter);
   eleventyConfig.addFilter('markdown', markdownFilter);
   eleventyConfig.addFilter('w3date', w3DateFilter);
 
-
-  // create collections
+  // Create collections
   const livePosts = p => p.date <= new Date() && !p.data.draft;
 
+  // Insights collection (filter drafts)
   eleventyConfig.addCollection("insights", function(collection) {
     return collection.getFilteredByGlob("src/site/insights/*.md")
             .filter(livePosts)
             .reverse();
   });
 
+  // Insight drafts collection (filter non-live posts)
   eleventyConfig.addCollection("insightDrafts", function(collection) {
     return collection.getFilteredByGlob("src/site/insights/*.md")
-            .filter( _ => !livePosts(_) )
+            .filter(_ => !livePosts(_))
             .reverse();
   });
 
-  eleventyConfig.addCollection("useCases", function(collection) {
-    return collection.getFilteredByGlob("src/site/use-cases/*.md")
-            .sort((a,b) => Math.sign(a.data.order - b.data.order));
+  // Create a collection for use cases, but based on the YAML file instead of Markdown
+  eleventyConfig.addCollection("useCases", function(collectionApi) {
+    // Access the global `use_cases` data from the YAML file
+    const useCasesData = collectionApi.getAll()[0].data.use_cases;
+
+    // Ensure the data is an array before sorting/returning
+    if (Array.isArray(useCasesData)) {
+      return useCasesData.sort((a, b) => Math.sign(a.order - b.order));
+    } else {
+      throw new Error("use_cases data is not an array");
+    }
   });
 
-
-  // This bit required for the dev command
+  // This bit is required for the dev command (to avoid ignoring .gitignore files)
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('src/site/static/js');
   eleventyConfig.addWatchTarget('src/site/static/css');
@@ -52,9 +60,9 @@ module.exports = function(eleventyConfig) {
       input: "src/site",
       output: "public"
     },
-    templateFormats : ["njk", "md"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
     passthroughFileCopy: true
   };
-}
+};
