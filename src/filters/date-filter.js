0a1c70262aa607f5428f61d622d7c48539c26c18
georgeunsworth <george@mortar.works>
2024-10-06 13:54:11 +0100

    Update date-filter.js

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,5 +1,3 @@
-// Stolen from hylia
-// Stolen from https://stackoverflow.com/a/31615643
 const appendSuffix = n => {
   var s = ['th', 'st', 'nd', 'rd'],
     v = n % 100;
@@ -9,8 +7,12 @@ const appendSuffix = n => {
 module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
 
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
   const dayWithSuffix = appendSuffix(dateObject.getDate());
 
-  return ${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()};
+  // Corrected template literal for string interpolation
+  return `${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
 };
